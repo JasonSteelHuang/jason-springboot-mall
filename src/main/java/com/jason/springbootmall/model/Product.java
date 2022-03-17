f8dea176a3e09d3e233c5f066c7340db1825a0f8
Jason <steel147896@gmail.com>
2022-03-18 01:09:30 +0800

    商品功能-使用Enum類型

--- src/main/java/com/jason/springbootmall/model/Product.java
+++ src/main/java/com/jason/springbootmall/model/Product.java
@@ -1,14 +1,15 @@
 package com.jason.springbootmall.model;
 
+import com.jason.springbootmall.constant.ProductCategory;
 import lombok.Data;
 
 import java.util.Date;
 
 @Data
-public class Product {
+public class  Product {
     private Integer productId;
     private String productName;
-    private String category;
+    private ProductCategory category;
     private String imageUrl;
     private Integer price;
     private Integer stock;
